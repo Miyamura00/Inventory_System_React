da13ec55cf6bd45cffe81abb8541c918fb6b96b6
Miyamura00 <iammartin00@gmail.com>
2025-08-25 16:01:50 +0800

    Update 8/25/2025

--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -1,6 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Assets = () => {
+
+    const [assets, setAssets] = useState([])
+    const [formData, setFormData] = useState({
+      modelName:"",
+      brandName:"",
+      purchaseDate:"",
+      status:"In Stock",
+      assignedTo:"",
+      category:"",
+      description:"",
+      serialNumber:"",
+      tag:""
+
+    })
+
+    const handleChange = (e) => {
+      const {name, value} = e.target;
+      setFormData((prev) => ({...prev,[name]:value}))
+    }
+    
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (assets.some((asset) => asset.serialNumber === formData.serialNumber)){
+        alert("Serial Number must be unique!")
+        return
+      }
+      if (assets.some((asset) => asset.tag === formData.tag)){
+        alert("Tag must be unique")
+        return
+      }
+
+      setAssets((prev) => [...prev, formData])
+      alert("Asset added sucessfully")
+
+      setFormData({
+      modelName:"",
+      brandName:"",
+      purchaseDate:"",
+      status:"In Stock",
+      assignedTo:"",
+      category:"",
+      description:"",
+      serialNumber:"",
+      tag:""
+      })
+        
+      
+
+    }
+    
+
   return (
     <div className="p-8"><h1 className="text-3xl font-bold">Assets</h1></div>
   )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 const Assets = () => {
   return (
 
-        <div className='flex'>
-      <div className="flex-1 min-h-screen bg-lightblue flex items-center
-      justify-center">
-        <h1 className="text-3x1 font-bold">The Greatest Cat Ever</h1>
-      </div>
-</div>
+      <div className="p-8"><h1 className="text-3xl font-bold">Dashboard</h1></div>
     
   )
 }
